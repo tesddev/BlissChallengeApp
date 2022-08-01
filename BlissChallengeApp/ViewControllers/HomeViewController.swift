725ba18b342d1791b45f74b50f3b4d210272cbe4
tesddev <tesleem.amuda@gmail.com>
2022-08-01 21:58:31 +0100

    fetch emoji list from api

--- BlissChallengeApp/ViewControllers/HomeViewController.swift
+++ BlissChallengeApp/ViewControllers/HomeViewController.swift
@@ -8,9 +8,35 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    lazy var getEmojiButton: AppButton = {
+        let button = AppButton()
+        button.setTitle("Get Emoji", for: .normal)
+        button.addTarget(self, action: #selector(didTapGetEmojiButton), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Constants.AppColors.backgroundColor
+        constraintViews()
+    }
+    
+    func constraintViews() {
+        view.addSubview(getEmojiButton)
+
+        NSLayoutConstraint.activate([
+            getEmojiButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            getEmojiButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -70),
+            getEmojiButton.heightAnchor.constraint(equalToConstant: 50),
+            getEmojiButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8)
+        ])
+    }
+
+    
+    @objc func didTapGetEmojiButton() {
+        NetworkManager.shared.fetchEmojis {[weak self] emojis in
+            print(emojis)
+        }
     }
 }
