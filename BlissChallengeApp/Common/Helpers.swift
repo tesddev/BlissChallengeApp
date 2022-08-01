c84f7ef70f3d31e4dc61770b16a3daf8b207bef0
tesddev <tesleem.amuda@gmail.com>
2022-08-01 21:42:40 +0100

    create app constant file

---
+++ BlissChallengeApp/Common/Helpers.swift
@@ -0,0 +1,16 @@
+//
+//  Helpers.swift
+//  BlissChallengeApp
+//
+//  Created by GIGL iOS on 01/08/2022.
+//
+
+import UIKit
+
+enum Constants{
+    enum AppColors {
+        static let backgroundColor = #colorLiteral(red: 0.2169797122, green: 0.3204716444, blue: 0.6406870484, alpha: 1)
+        static let navBarColor = #colorLiteral(red: 0.2645595968, green: 0.3515060246, blue: 0.6144788861, alpha: 1)
+    }
+}
+
--- BlissChallengeApp/ViewControllers/HomeViewController.swift
+++ BlissChallengeApp/ViewControllers/HomeViewController.swift
@@ -11,7 +11,6 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = Constants.AppColors.backgroundColor
     }
 }
-
